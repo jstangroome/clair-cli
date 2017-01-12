ea9338cf23b8b7ece9b7649e538ab2dfe9274e7c
Jason Stangroome <jason@section.io>
2017-01-12 21:49:05 +1100

    Clean Clair data between runs

--- build.sh
+++ build.sh
@@ -13,4 +13,7 @@ docker build -t jstangroome/clair-cli ./
 kubernetes_ip=$(minikube ip)
 clair_port=30060
 
+root_layer=sha256:75a822cd7888e394c49828b951061402d31745f596b1f502758570f2d0ee79e2
+#curl -sS -XDELETE "http://${kubernetes_ip}:${clair_port}/v1/layers/${root_layer}"
+
 docker run --rm -e CLAIR_HOSTNAME="${kubernetes_ip}" -e CLAIR_PORT="${clair_port}" -i jstangroome/clair-cli node:6.9
